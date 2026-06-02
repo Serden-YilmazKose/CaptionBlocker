ea8b6d4a1c64bacb8c3629b43d18e2a3196b41ca
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2026-06-02 12:57:38 +0300

    setup.sh: Added colors to script.

--- setup.sh
+++ setup.sh
@@ -7,6 +7,22 @@ reqs="requirements.txt"
 user="root"
 pass="pass"
 db="caption_capper"
+dbg_page="about:debugging#/runtime/this-firefox"
+
+# Colors
+BLANK='\033[0m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+
+# Set color, print text, the reset color
+echo_col() {
+	[ "$#" -ne 2 ] && echo "echo_col: Requires 2 arguments." && exit 1
+	col="$1"
+	text="$2"
+	echo -ne "$col"
+	echo "$text"
+	echo -ne "$BLANK"
+}
 
 # Print bar of this shape to stdout: <====>
 bar() {
@@ -22,47 +38,53 @@ bar() {
 isInstalled() {
 	binary="$1"
 	[ -n "$binary" ] || exit 1
-	which "$binary" > /dev/null 2>&1 || { echo "Needed package: $binary" && exit 1; }
+	which "$binary" > /dev/null 2>&1 || { echo_col  "$RED" "Needed package: $binary" && exit 1; }
 }
 
-# Check if these are installed
+# # Check if these are installed
 echo "Checking if required binaries are on system..."
-# isInstalled firefox
-# isInstalled mariadb
-# isInstalled pip
-# isInstalled python
-echo "Required binaries are on system!"
+isInstalled firefox
+isInstalled mariadb
+isInstalled pip
+isInstalled python
+echo_col "$GREEN" "Required binaries are on system!"
 bar
 
-# Create Python virtual environment, then source
+# # Create Python virtual environment, then source
 echo "Creating Python virtual environment..."
-# python -m venv "$venv"
-# source "$venv"/bin/activate
-echo "Python virtual environment created!"
+python -m venv "$venv"
+source "$venv"/bin/activate
+echo_col  "$GREEN" "Python virtual environment created!"
 bar
 
-# Install all requirements
+# # Install all requirements
 echo "Pip installing Python packages..."
-# pip install -r "$reqs"
-echo "Python packages pip installed!"
+pip install -r "$reqs"
+echo_col  "$GREEN" "Python packages pip installed!"
 bar
 
-# Check if MariaDB is running
+# # Check if MariaDB is running
 echo "Checking if MariaDB is running..."
 while true; do
 	mdb_status=$(mariadb --user="$user" --password="$pass" < status.sql > /dev/null 2>&1)
 	[ "$?" -eq 0 ] && break || systemctl start mariadb
 done
-echo "MariaDB is running!"
+echo_col  "$GREEN" "MariaDB is running!"
 bar
 
-# Create MariaDB database, if it doesn't exist
+# # Create MariaDB database, if it doesn't exist
 create_db() {
 	echo "Creating database..."
 	mariadb --user="$user" --password="$pass" < create_db.sql
 	mariadb --user="$user" --password="$pass" < create_table.sql
-	echo "Database created!"
+	echo_col "$GREEN" "Database created!"
 }
 echo "Checking if database exists..."
-mariadb --user="$user" --password="$pass" < script.sql | grep -q "$db" && echo "Database already exists!" || create_db
+mariadb --user="$user" --password="$pass" < script.sql | grep -q "$db" && echo_col  "$GREEN" "Database already exists!" || create_db
+bar
+
+# Open Firefox debugging page
+echo "Opening extension debugging page..."
+firefox "$dbg_page"
+echo_col "$GREEN" "Extension debugging page opened!"
 bar
